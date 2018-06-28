03661f1cd56ca37dd1878d33afef614d6b59e62d
Ben Johnson <bkj.322@gmail.com>
2018-06-28 17:51:46 -0400

    python3 compatability

--- hyperband.py
+++ hyperband.py
@@ -9,6 +9,8 @@
     good as far as black-box optimization goes, so let's try to do it faster.
 """
 
+from __future__ import print_function, division
+
 import sys
 import numpy as np
 import ujson as json
@@ -22,7 +24,7 @@ class HyperBand:
         
         self.max_iter = max_iter
         self.eta = eta
-        self.s_max = int(log(max_iter) / log(eta))
+        self.s_max = int(log(max_iter) // log(eta))
         self.B = (self.s_max + 1) * max_iter  
         
         self.best_obj = np.inf
@@ -32,7 +34,7 @@ class HyperBand:
         for s in reversed(range(self.s_max + 1)):
             
             # initial number of configs
-            n = int(ceil(self.B / self.max_iter / (s + 1) * self.eta ** s)) 
+            n = int(ceil(self.B // self.max_iter // (s + 1) * self.eta ** s)) 
             
             # initial number of iterations per config
             r = self.max_iter * self.eta ** (-s) 
@@ -45,20 +47,19 @@ class HyperBand:
                 # number of iterations for these configs
                 r_i = r * self.eta ** i
                 
-                print >> sys.stderr, "\n -- %d configs @ %d iterations -- \n" % (len(configs), int(round(r_i)))
+                print("\n -- %d configs @ %d iterations -- \n" % (len(configs), int(round(r_i))), file=sys.stderr)
                 
                 results = []
                 for j, config in enumerate(configs):
-                    print >> sys.stderr, "Config %d: %s" % (j, json.dumps(config))
+                    print("Config %d: %s" % (j, json.dumps(config)), file=sys.stderr)
                     
                     res = self.model.eval_config(config=config, iters=r_i)
                     results.append(res)
                     
                     self.best_obj = min(res['obj'], self.best_obj)
-                    print >> sys.stderr, \
-                        "Current: %f | Best: %f" % (float(res['obj']), self.best_obj)
+                    print("Current: %f | Best: %f" % (float(res['obj']), self.best_obj), file=sys.stderr)
                     
-                    print json.dumps(res)
+                    print(json.dumps(res))
                     sys.stdout.flush()
                 
                 self.history += results
@@ -67,7 +68,7 @@ class HyperBand:
                 results = sorted(results, key=lambda x: x['obj'])
                 
                 # Drop models that have already converged
-                results = filter(lambda x: not x.get('converged', False), results)
+                results = list(filter(lambda x: not x.get('converged', False), results))
                 
                 # Determine how many configs to keep
                 n_keep = int(n * self.eta ** (-i - 1))
